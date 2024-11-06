83c5766d6f9e097395e08003168cdf11c37710de
Ken-Hsu-1 <gtr0307.ken@gmail.com>
2024-11-06 17:57:45 +0800

    option_a

--- Lab5/Core/Src/main.c
+++ Lab5/Core/Src/main.c
@@ -438,7 +438,7 @@ static void MX_TIM3_Init(void)
   htim3.Instance = TIM3;
   htim3.Init.Prescaler = 9999;
   htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim3.Init.Period = 7999;
+  htim3.Init.Period = 15999;
   htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
